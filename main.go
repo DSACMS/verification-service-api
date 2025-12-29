3856494a839fcc7f82aa80c519f476201697a7aa
Jake Shilling <shilling.jake@gmail.com>
2025-12-29 11:40:02 -0500

    Add logging lint to golangci-lint

--- main.go
+++ main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
 
 	"github.com/DSACMS/verification-service-api/internal/config"
+	"github.com/DSACMS/verification-service-api/internal/logger"
 	"github.com/DSACMS/verification-service-api/internal/middleware"
 	"github.com/DSACMS/verification-service-api/internal/otel"
 	"github.com/DSACMS/verification-service-api/internal/router"
@@ -20,40 +20,70 @@ import (
 )
 
 func main() {
-	log.Printf("Config: %+v\n", config.AppConfig)
+	err := run()
+	if err != nil {
+		os.Exit(1)
+	}
+
+	os.Exit(0)
+}
+
+func run() error {
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
 	defer stop()
 
 	shutdownOtel, err := otel.InitOtel(ctx)
 	if err != nil {
-		log.Println(err)
+		logger.Logger.ErrorContext(
+			ctx,
+			"Otel error",
+			"err",
+			err,
+		)
+		return err
 	}
-	defer func() {
-		shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-		defer cancel()
-		shutdownErr := shutdownOtel(shutdownCtx)
-		if shutdownErr != nil {
-			log.Printf("Error during shutdown: %v", shutdownErr)
-		}
-	}()
-
-	_, span := otel.Tracer.Start(ctx, "startup")
-	span.AddEvent("Starting up")
-	span.End()
+	if otelShutdown != nil {
+		defer func() {
+			shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+			defer cancel()
+			shutdownErr := otelShutdown(shutdownCtx)
+			logger.Logger.ErrorContext(
+				shutdownCtx,
+				"Error shutting down otel",
+				"err",
+				shutdownErr,
+			)
+		}()
+	}
 
 	app, err := buildApp()
 	if err != nil {
-		log.Printf("Failed to build app: %v\n", err)
-		return
+		logger.Logger.ErrorContext(
+			ctx,
+			"Error building app",
+			"err",
+			err,
+		)
+		return err
 	}
 
 	if err := runServer(ctx, app, ":8000"); err != nil {
-		log.Printf("server error: %v", err)
+		logger.Logger.ErrorContext(
+			ctx,
+			"Server error",
+			"err",
+			err,
+		)
+		return err
 	}
+
+	return nil
 }
 
 func buildApp() (*fiber.App, error) {
 	app := fiber.New()
+
+	app.Use(logger.Middleware)
 
 	app.Use(cors.New(cors.Config{
 		AllowOrigins: "*",
